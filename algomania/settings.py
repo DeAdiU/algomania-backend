86d668f4d2de196596daeadb134ad253023a6b80
DeAdiU <uttarwaraditya29@gmail.com>
2024-08-04 18:51:51 +0530

    adding whitenoise

--- algomania/settings.py
+++ algomania/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'whitenoise.runserver_nostatic'
     'api',
     'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS=os.path.join(BASE_DIR,'static') 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_URL='/static/'
+STATIC_DIRS=[os.path.join(BASE_DIR,'static') ]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
